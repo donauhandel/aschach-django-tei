01dc60bc7371c2bdb2cf755595947143e35cabce
csae8092 <peter.andorfer@oeaw.ac.at>
2020-04-27 11:48:16 +0200

    added idno and coordinates to tei serialisation of places

--- tei/places.py
+++ tei/places.py
@@ -17,9 +17,22 @@ class TeiPlace():
         ] = f"place__{self.res.id}"
         name = ET.Element("{http://www.tei-c.org/ns/1.0}placeName")
         name.text = self.res.name
+        item.append(name)
         if self.res.region:
             region_el = ET.Element("{http://www.tei-c.org/ns/1.0}region")
             region_el.text = self.res.region.name
-            name.append(region_el)
-            item.append(name)
+            item.append(region_el)
+        if self.res.lat:
+            loc_el = ET.Element("{http://www.tei-c.org/ns/1.0}location")
+            geo_el = ET.Element("{http://www.tei-c.org/ns/1.0}geo")
+            item.append(loc_el)
+            loc_el.append(geo_el)
+            geo_el.attrib['decls'] = 'LatLng'
+            geo_el.text = f"{self.res.lat} {self.res.lng}"
+        for x in self.res.normdata_url.all():
+            idno_el = ET.Element("{http://www.tei-c.org/ns/1.0}idno")
+            idno_el.attrib['subtype'] = x.domain.replace(' ', '').strip().lower()
+            idno_el.attrib['type'] = 'URL'
+            idno_el.text = x.uri
+            item.append(idno_el)
         return item
