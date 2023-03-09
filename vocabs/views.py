542c53d72e3c33544870abe971ac75dceab5fa20
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-09 20:26:52 +0100

    started with linting #9

--- vocabs/views.py
+++ vocabs/views.py
@@ -1,10 +1,10 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import DeleteView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django_tables2 import SingleTableView, RequestConfig
+from django_tables2 import RequestConfig
 from .models import SkosConcept, SkosConceptScheme, SkosLabel, SkosCollection, Metadata
 from .forms import *
 from .tables import *
