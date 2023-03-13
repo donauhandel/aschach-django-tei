cf76e95a61f6a53a4b23bdb3f7ac2b21f7b51154
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-13 11:17:07 +0100

    added/renamed denormalizing Angabe script

--- aschach/management/commands/denormalize_angaben.py
+++ aschach/management/commands/denormalize_angaben.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     # Show this when the user types help
-    help = "denormalizes Angabe-Ware"
+    help = "denormalizes Angabe with Waren, Personen und Orten"
 
     # A command must define handle()
     def handle(self, *args, **options):
@@ -13,4 +13,8 @@ class Command(BaseCommand):
         for x in tqdm(items, total=items.count()):
             waren = x.get_waren()
             x.related_good.set(waren)
+            persons = x.get_persons
+            x.related_person.set(persons)
+            places = x.get_places
+            x.related_place.set(places)
         print("done")
