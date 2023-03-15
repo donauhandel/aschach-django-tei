e6f2fe4ae91b65cf25baca71ef716c3e294e7146
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-15 09:49:14 +0100

    first try of gh-arche-ingest wf

--- ingest.sh
+++ ingest.sh
@@ -1,3 +1,5 @@
 # composer require acdh-oeaw/arche-ingest
 python manage.py arche
 vendor/bin/arche-import-metadata ./media/tei_out/arche.ttl http://127.0.0.1/api username password --retriesOnConflict 25
+
+vendor/bin/arche-import-binary ./to_ingest https://id.acdh.oeaw.ac.at/donauhandel-aschach http://127.0.0.1/api username password --skip not_exist
