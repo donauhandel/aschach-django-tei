f7e49bb6fd9d8c22c7e7d37ede817d25ca2b8733
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-15 10:04:01 +0100

    minor

--- ingest.sh
+++ ingest.sh
@@ -1,5 +1,7 @@
 # composer require acdh-oeaw/arche-ingest
-python manage.py arche
-vendor/bin/arche-import-metadata ./media/tei_out/arche.ttl http://127.0.0.1/api username password --retriesOnConflict 25
+ARCHE=http://127.0.0.1/api
+PROJECT_NAME=donauhandel-aschach
 
-vendor/bin/arche-import-binary ./to_ingest https://id.acdh.oeaw.ac.at/donauhandel-aschach http://127.0.0.1/api username password --skip not_exist
+python manage.py arche
+vendor/bin/arche-import-metadata ./media/tei_out/arche.ttl ${ARCHE} username password --retriesOnConflict 25
+# vendor/bin/arche-import-binary ./to_ingest https://id.acdh.oeaw.ac.at/${PROJECT_NAME} http://127.0.0.1/api username password --skip not_exist
