f7d4808594774126fec93d71246e2e8eef7459d8
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-28 09:40:23 +0200

    typos, descriptions updates

--- ingest.sh
+++ ingest.sh
@@ -5,6 +5,6 @@ ARCHE_LOGIN="${ARCHE_LOGIN:=username}"
 ARCHE_PASSWORD="${ARCHE_PASSWORD:=password}"
 
 
-# python manage.py arche
+python manage.py arche
 vendor/bin/arche-import-metadata ./media/tei_out/arche.ttl ${ARCHE} ${ARCHE_LOGIN} ${ARCHE_PASSWORD} --retriesOnConflict 25
 # vendor/bin/arche-import-binary ./to_ingest https://id.acdh.oeaw.ac.at/${PROJECT_NAME} ${ARCHE} ${ARCHE_LOGIN} ${ARCHE_PASSWORD} --skip not_exist
